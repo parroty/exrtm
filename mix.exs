1a507a62e189bdd1a6703a8204b55cd8d34125f6
parroty <parroty00@gmail.com>
2013-08-06 20:02:13 +0900

    add settings for ex_doc

--- mix.exs
+++ mix.exs
@@ -5,6 +5,9 @@ defmodule Exrtm.Mixfile do
     [ app: :exrtm,
       version: "0.0.1",
       elixir: "~> 0.10.0",
+      name: "exrtm",
+      source_url: "https://github.com/parroty/exrtm",
+      homepage_url: "https://github.com/parroty/exrtm",
       deps: deps ]
   end
 
@@ -16,6 +19,9 @@ defmodule Exrtm.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [{:mock, ">= 0.0.3", [github: "jjh42/mock"]}]
+    [
+      { :mock, ">= 0.0.3", [github: "jjh42/mock"] },
+      { :ex_doc, github: "elixir-lang/ex_doc" }
+    ]
   end
 end
