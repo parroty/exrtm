e3e47c922b7862f0e6b8b61830f07186197d4412
parroty <parroty00@gmail.com>
2013-08-09 00:11:32 +0900

    add package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "exrtm", description: "An elixir library for remember the milk API",
+                 version: "0.0.1", keywords: ["Elixir","remember the milk","RTM", "REST"],
+                 maintainers: [[name: "parroty", email: "parroty00@gmail.com"]],
+                 repositories: [[github: "parroty/exrtm"]])
