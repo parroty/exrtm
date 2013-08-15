8288ae8e62a514f7ace4911c17b11b3f8eb9bb0b
parroty <parroty00@gmail.com>
2013-08-16 00:42:07 +0900

    remove Chunk reference

--- test/exrtm/protocol_test.exs
+++ test/exrtm/protocol_test.exs
@@ -6,7 +6,6 @@ defmodule Exrtm.ProtocolTest do
 
   alias Exrtm.Record.List
   alias Exrtm.Record.Task
-  alias Exrtm.Record.Chunk
 
   test "Exrtm.puts for List" do
     list = List.new(id: 1, name: "test_list")
@@ -16,15 +15,15 @@ defmodule Exrtm.ProtocolTest do
   end
 
   test "Exrtm.puts for Task" do
-    task = Task.new(id: 2, name: "test_task", chunks: [Chunk.new, Chunk.new])
+    task = Task.new(id: 2, name: "test_task")
     assert capture_io(fn ->
       Exrtm.puts(task)
     end) == "test_task\n"
   end
 
   test "Exrtm.puts for Task list" do
-    task1 = Task.new(id: 2, name: "test_task1", chunks: [Chunk.new, Chunk.new])
-    task2 = Task.new(id: 3, name: "test_task2", chunks: [Chunk.new, Chunk.new])
+    task1 = Task.new(id: 2, name: "test_task1")
+    task2 = Task.new(id: 3, name: "test_task2")
     assert capture_io(fn ->
       Exrtm.puts([task1, task2])
     end) == "test_task1\ntest_task2\n"
